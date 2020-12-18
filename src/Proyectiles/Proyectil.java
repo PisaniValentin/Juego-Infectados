0b21b69dcdd1219dc1c7ad3a81f2681bc6184183
nahueld97 <nahuedl97@outlook.com>
2020-12-17 22:52:50 -0300

    cambios

--- src/Proyectiles/Proyectil.java
+++ src/Proyectiles/Proyectil.java
@@ -12,10 +12,6 @@ public abstract class Proyectil extends GameObject {
 		this.rango = rango;
 		this.daño = daño;
 		this.punto = punto;
-//		ImageIcon icono_imagen = new ImageIcon(this.getClass().getClassLoader().getResource("Imagenes/disparo.gif"));
-//		imagen = new JLabel(icono_imagen);
-//		ancho = icono_imagen.getIconWidth();
-//		alto = icono_imagen.getIconHeight();
 	}
 
 	public void remove() {
